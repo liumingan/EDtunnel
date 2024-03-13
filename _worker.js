5768b423178a3dc657ca832ac19aff50c12a71b9
liumingan <31801071+liumingan@users.noreply.github.com>
2024-03-14 02:13:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '84c983bc-36c7-4a5f-9fd7-2beec4cade9a';
 
-const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.bestip.one'];
+const proxyIPs = ['work1.liumingan.cloudns.org', 'work2.liumingan.cloudns.org', 'work3.liumingan.cloudns.org', 'work4.liumingan.cloudns.org', 'work5.liumingan.cloudns.org', 'work6.liumingan.cloudns.org', 'work7.liumingan.cloudns.org'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
